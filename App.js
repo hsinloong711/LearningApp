f3c9fec9cab8724be0ec3af165a8d06cdcd155a5
Gary <hsinloong711@outlook.com>
2022-05-31 11:38:42 +0800

    State Hooks

--- App.js
+++ App.js
@@ -1,26 +1,39 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, View, Button, Linking} from 'react-native';
 
 const App = () => {
+  const [name, setName] = useState('Gary');
+  const [session, setSession] = useState({number: 71, title: 'ryze'});
+  const [current, setCurrent] = useState(true);
+  const [counter, setCounter] = useState(10);
+
+  const onClickHandler = () => {
+    setName('I am Gary');
+    setSession({
+      number: 711,
+      title: 'Vladimir',
+    });
+    setCurrent(false);
+  };
+
+  const onClickHandler1 = () => {
+    setCounter(counter + 1);
+  };
+
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>I am Gary</Text>
-      <Button
-        title="Youtube Channel"
-        onPress={() => {
-          Linking.openURL(
-            'https://www.youtube.com/watch?v=ANdSdIlgsEw&t=2426s&ab_channel=ProgrammingwithMash',
-          );
-        }}
-      />
+      <Text style={styles.text}>{name}</Text>
+      <Text style={styles.text}>
+        {session.number} and {session.title}
+      </Text>
+      <Text style={styles.text}>
+        {current ? 'current session true' : 'next session false'}
+      </Text>
+      <Text style={styles.text}>
+        Number:{counter * 5} Click:{counter}
+      </Text>
+      <Button title="Update Number" onPress={onClickHandler1} />
+      <Button title="Update State" onPress={onClickHandler} />
     </View>
   );
 };
